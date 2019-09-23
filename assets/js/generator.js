40c97c497f8e2b4c2c18c8959702c6deed8dafa0
Dzénetan Massart <46716929+DzenetanMassart@users.noreply.github.com>
2019-09-23 16:28:55 +0200

    Add files via upload

---
+++ assets/js/generator.js
@@ -0,0 +1,71 @@
+let gen=" ";
+/*
+|===================|
+|L'affichage de base|
+|===================|
+*/
+for (let ii = 0; ii < softs.length; ii++) {
+
+    let softwares = (softs[ii]);
+gen+='<a href="'+softwares.link+'"><div class="card" style="width: 18rem;">';
+gen+='<img src="'+softwares.img_link +'" class="card-img-top"';
+gen+=' alt="'+softwares.desc+'">';
+gen+='<div class="card-body">';
+gen+='<p class="card-text">'+softwares.desc+'</p>';
+gen+='</div></div></a>';
+
+document.getElementById("gen").innerHTML = gen;
+
+}
+/*
+|================================|
+|Les différents boutons du header|
+|================================|
+*/
+
+// Les boutons du header et liens à leurs fonctions
+document.getElementById("all").addEventListener("click", all);
+
+document.getElementById("Image").addEventListener("click", function() { generator("Image"); });
+document.getElementById("Video").addEventListener("click", function() { generator("Video"); });
+document.getElementById("Audio").addEventListener("click", function() { generator("Audio"); });
+document.getElementById("tD").addEventListener("click", function() { generator("tD"); });
+document.getElementById("Code").addEventListener("click", function() { generator("Code"); });
+
+// Les fonctions des boutons
+function all(){
+    gen='';
+    for (let ii = 0; ii < softs.length; ii++) {
+
+        let softwares = (softs[ii]);
+    gen+='<a href="'+softwares.link+'"><div class="card" style="width: 18rem;">';
+    gen+='<img src="'+softwares.img_link +'" class="card-img-top"';
+    gen+=' alt="'+softwares.desc+'">';
+    gen+='<div class="card-body">';
+    gen+='<p class="card-text">'+softwares.desc+'</p>';
+    gen+='</div></div></a>';
+    
+    document.getElementById("gen").innerHTML = gen;
+    
+    }
+}
+
+
+function generator(nom) {
+
+    document.title = "MoW" + nom;
+    gen = " ";
+
+    for (let ii = 0; ii < softs.length; ii++) {
+
+        let softwares = (softs[ii]);
+        if(softwares.type==nom){
+    gen+='<a href="'+softwares.link+'"><div class="card" style="width: 18rem;">';
+    gen+='<img src="'+softwares.img_link +'" class="card-img-top"';
+    gen+=' alt="'+softwares.desc+'">';
+    gen+='<div class="card-body">';
+    gen+='<p class="card-text">'+softwares.desc+'</p>';
+    gen+='</div></div></a>';
+        }}
+    document.getElementById("gen").innerHTML = gen;
+    }
---
+++ assets/js/softs.js
@@ -0,0 +1,96 @@
+//Les différents types: Image   Video   Audio   tD   Code
+
+
+let softs=[
+    // Les logiciels d'image
+{
+    "type":"Image",
+    "img_link": "https://pic.clubic.com/v1/images/1692381/raw",
+    "desc": "Le Photoshop Online.",
+    "link": "https://www.photopea.com/"
+},  
+{
+    "type":"Image",
+    "img_link": "https://www.remove.bg/images/logo.svg",
+    "desc": "Ce logiciel retire le fond des images",
+    "link": "https://www.remove.bg/"
+},  
+{
+    "type":"Image",
+    "img_link": "https://www198.lunapic.com/editor/images/lunapic-bw.gif",
+    "desc": "Un gros site permettant de faire pleins d'effets sur les images... plus complet que SnapChat et Instagram.",
+    "link": "https://www198.lunapic.com/editor/"
+},  
+{
+    "type":"Image",
+    "img_link": "https://compressor.io/img/compressor.png",
+    "desc": "Pour compresser sans pertes de qualité les images.",
+    "link": "https://compressor.io/"
+}, 
+{
+    "type":"Image",
+    "img_link": "https://www.iloveimg.com/img/iloveimg.svg",
+    "desc": "Pour redimensionner plusieurs images à la fois.",
+    "link": "https://www.iloveimg.com/fr/redimensionner-image"
+}, 
+
+// Les logiciels de vidéo
+{
+    "type":"Video",
+    "img_link": "https://panzoid.com/img/logo.svg",
+    "desc": "Un éditeur de vidéo assez complet et sans watermark.",
+    "link": "https://panzoid.com/tools/videoeditor"
+},   
+{
+    "type":"Video",
+    "img_link": "https://fr.savefrom.net/img/logo_small2.svg",
+    "desc": "Pour télécharger des vidéos depuis plusieurs sites.",
+    "link": "https://fr.savefrom.net"
+},  
+
+// Les logiciels pour l'audio
+{
+    "type":"Audio",
+    "img_link": "https://www.ispeech.org/img/logos/ispeech.png",
+    "desc": "Un text-to-speech assez complet et facile à utiliser avec Audacity.",
+    "link": "https://www.ispeech.org/text.to.speech"
+},   
+{
+    "type":"Audio",
+    "img_link": "https://soundation.com/assets/soundation-icon-light.svg?v=2",
+    "desc": "Logiciel partiellement gratuit pour faire de la musique.",
+    "link": "https://soundation.com/"
+}, 
+// Les logiciel de 3D
+{
+    "type":"tD",
+    "img_link": "https://labs.sketchfab.com/sculptfab/ressources/logo.png",
+    "desc": "Le logiciel de création 3D de Sketchfab.\n Tout commence par une boule.",
+    "link": "https://labs.sketchfab.com/sculptfab/"
+},  
+{
+    "type":"tD",
+    "img_link": "http://medias.3dvf.com/news/logiciels/mixamo/mixamo.png",
+    "desc": "Un logiciel pour animer des modèles 3D humanoïdes.",
+    "link": "https://www.mixamo.com/#/"
+},  
+// Les logiciels de Coding
+{
+    "type":"Code",
+    "img_link": "http://canvimation.github.io/assets/logo.png",
+    "desc": "Pour générer des dessins en Canvas pour les sites en Javascript.",
+    "link": "http://canvimation.github.io/"
+},
+{
+    "type":"Code",
+    "img_link": "https://digitalworkshopcenter.com/wp-content/uploads/2018/02/JavaScript-Classes-at-Digital-Workshop-Center-2.png",
+    "desc": "Connaître les KeyCodes pour Javascript.",
+    "link": "https://keycode.info/"
+},
+{
+    "type":"Code",
+    "img_link": "https://caffeinecreations.ca/uploads/blog/_1200x630_crop_center-center_82_none/emmet-logo.jpg?mtime=1553958970",
+    "desc": "L'ensemble des raccourcis Emmet pour Visual Studio Code.",
+    "link": "https://docs.emmet.io/cheat-sheet/"
+}
+];
